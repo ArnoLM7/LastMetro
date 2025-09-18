4e8c45c973718506e0c481ec38ad5d0b1e3b3fa5
Arno_lemoil <arno.lemoil@gmail.com>
2025-09-18 12:40:57 +0200

    add-postgresql

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  return res.status(200).send({
-    message: "Hello World!",
-  });
+	return res.status(200).send({
+		message: "Hello World!",
+	});
 });
 
 app.listen(port, () => {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
 
 module.exports = app;
