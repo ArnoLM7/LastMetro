f23ab15333489f41665e544defc0173c44d6cf43
Arno_lemoil <arno.lemoil@gmail.com>
2025-10-06 16:32:49 +0200

    ajout env jest dans eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import js from "@eslint/js";
-import globals from "globals";
+import globals, { jest } from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+	{
+		files: ["**/*.{js,mjs,cjs,jsx}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+		languageOptions: { globals: globals.browser },
+		env: { jest: true, node: true },
+	},
+	pluginReact.configs.flat.recommended,
 ]);
