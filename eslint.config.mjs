c00bd59fd00374bbcf8963646f691cd1f9c9e5df
Arno_lemoil <arno.lemoil@gmail.com>
2025-10-07 12:05:47 +0200

    correction nodeGlobals eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,21 @@
-import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
+const { jest: jestGlobals, node: nodeGlobals } = globals;
+
+export default [
 	{
-		files: ["**/*.{js,mjs,cjs,jsx}"],
-		plugins: { js },
-		extends: ["js/recommended"],
-		languageOptions: { globals: globals.browser },
-		env: { jest: true, node: true },
+		files: ["**/*.js"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "module",
+			globals: {
+				...nodeGlobals,
+				...jestGlobals,
+			},
+		},
+		rules: {
+			"no-unused-vars": "warn",
+			"no-console": "off",
+		},
 	},
-	pluginReact.configs.flat.recommended,
-]);
+];
